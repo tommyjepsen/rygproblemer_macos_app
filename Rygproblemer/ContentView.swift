2986afe8e225376e2f10207f0e56191ba57360f0
Tommy <tommy@tommyjepsen.com>
2021-03-22 09:54:50 +0100

    Initial Commit

---
+++ Rygproblemer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Rygproblemer
+//
+//  Created by Tommy Jepsen on 22/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
